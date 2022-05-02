3427f153e15f1adb3f584d6926df2553409cf120
lex-art <drummerkoka@gmail.com>
2022-05-01 21:28:16 -0600

    fixed port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Response} from 'express';
 const app = express();
 app.use(express.json()); // middleware que transforma un body a un json
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (_, res: Response) => {
     res.send('Hello world');
